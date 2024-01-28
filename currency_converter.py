548422e7b21fa10575488b00b6510d43e28cd3c5
Haneena <154798336+ayshahaneena@users.noreply.github.com>
2024-01-28 14:47:22 +0530

    Add files via upload

---
+++ currency_converter.py
@@ -0,0 +1,10 @@
+
+def main():
+    print("This program coverts us dollars into indian Rupees")
+
+    dollar = eval(input("Enter amount in dollars "))
+    converter= dollar * 83.11
+    print(f"That is {converter} Indian Rupees")
+
+
+main()
